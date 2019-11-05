68e8f935e8d801d77a6a88be09a87079e3bf87e0
ice <grxshen@hotmail.com>
2019-11-06 07:02:55 +0800

    add matrix2

--- main_matrix.py
+++ main_matrix.py
@@ -1,4 +1,5 @@
 from playLA.Matrix import Matrix
+from playLA.Vector import Vector
 
 if __name__ == "__main__":
     matrix = Matrix([[1, 2], [3, 4]])
@@ -9,5 +10,15 @@ if __name__ == "__main__":
     matrix2 = Matrix([[5, 6], [7, 8]])
     print("add: {}".format(matrix + matrix2))
     print("sub: {}".format(matrix - matrix2))
-
     print("zero_2_3: {}".format(Matrix.zero(2, 3)))
+
+    T = Matrix([[1.5, 0], [0, 2]])
+    p = Vector([5, 3])
+    print("T.dot(p) = {}".format(T.dot(p)))
+
+    P = Matrix([[0, 4, 5], [0, 0, 3]])
+    print("T.dot(P) = {}".format(T.dot(P)))
+
+    print("A.dot(B) = {}".format(matrix.dot(matrix2)))
+    print("B.dot(A) = {}".format(matrix2.dot(matrix)))
+
